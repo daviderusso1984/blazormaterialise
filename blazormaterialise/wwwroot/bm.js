5b7d93fe1fb2c71eb8a94d733e7a6665f4164e16
Davide Russo <daviderusso1984@outlook.it>
2020-06-18 20:36:08 +0200

    first

---
+++ blazormaterialise/wwwroot/bm.js
@@ -0,0 +1,17 @@
+
+
+
+  function media_init(){
+    var elems = document.querySelectorAll('.materialboxed');
+    var options={};
+    var instances = M.Materialbox.init(elems, options);
+  }
+
+  function carousel_init(){
+    var elems = document.querySelectorAll('.carousel');
+    var options={};
+    var instances = M.Carousel.init(elems, options);
+  }
+
+  
+  
